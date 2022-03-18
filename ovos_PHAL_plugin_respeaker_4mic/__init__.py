0f54c45b1d7e1d461053dfd617522ee4e59a80ba
jarbasai <jarbasai@mailfence.com>
2022-03-18 11:04:51 +0000

    drivers

--- ovos_PHAL_plugin_respeaker_4mic/__init__.py
+++ ovos_PHAL_plugin_respeaker_4mic/__init__.py
@@ -1,5 +1,5 @@
 from gpiozero import LED
-from pixel_ring import pixel_ring
+from ovos_PHAL_plugin_respeaker_4mic.drivers import get_pixel_ring
 from time import sleep
 
 from ovos_PHAL.detection import is_respeaker_4mic
@@ -18,34 +18,35 @@ class Respeaker4MicControlPlugin(PHALPlugin):
         super().__init__(bus=bus, name="ovos-PHAL-plugin-respeaker-4mic", config=config)
         self.power = LED(5)
         self.power.on()
-        pixel_ring.set_brightness(10)
-        pixel_ring.change_pattern('echo')
-        pixel_ring.wakeup()
+        self.pixel_ring = get_pixel_ring()
+        self.pixel_ring.set_brightness(10)
+        self.pixel_ring.change_pattern('echo')
+        self.pixel_ring.wakeup()
         sleep(1.5)
-        pixel_ring.off()
+        self.pixel_ring.off()
 
     def on_record_begin(self, message=None):
-        pixel_ring.listen()
+        self.pixel_ring.listen()
 
     def on_record_end(self, message=None):
         self.on_reset(message)
 
     def on_audio_output_start(self, message=None):
-        pixel_ring.speak()
+        self.pixel_ring.speak()
 
     def on_audio_output_end(self, message=None):
         self.on_reset(message)
 
     def on_think(self, message=None):
-        pixel_ring.think()
+        self.pixel_ring.think()
 
     def on_reset(self, message=None):
-        pixel_ring.off()
+        self.pixel_ring.off()
 
     def on_system_reset(self, message=None):
         self.on_reset(message)
 
     def shutdown(self):
-        pixel_ring.off()
+        self.pixel_ring.off()
         self.power.off()
         super().shutdown()
